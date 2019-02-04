90ec90cd414e215a381bede99c7b961e434612d5
Rudy Ravelin <rudy@noukod.com>
2019-02-04 16:48:55 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,34 +3,34 @@ class PostsController < ApplicationController
   # def index
   #   @posts = Post.all
   # end
-  # 
+  #
   # def show
   #   @post = Post.find(params[:id])
   # end
-  # 
+  #
   # def new
   #   @post = Post.new
   # end
-  # 
+  #
   # def create
   #   @post = Post.new(params)
   #   @post.save
   #   redirect_to post_path(@post)
   # end
-  # 
+  #
   # def update
   #   @post = Post.find(params[:id])
   #   @post.update(params.require(:post))
   #   redirect_to post_path(@post)
   # end
-  # 
+  #
   # def edit
   #   @post = Post.find(params[:id])
   # end
   def index
   # provide a list of authors to the view for the filter control
   @authors = Author.all
- 
+
   # filter the @posts list based on user input
   if !params[:author].blank?
     @posts = Post.by_author(params[:author])
